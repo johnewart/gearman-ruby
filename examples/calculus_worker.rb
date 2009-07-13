53bcda2792f1d47bdd0df514a00cd37e21468332
Kim Altintop <kim.altintop@xing.com>
2009-07-13 14:34:49 +0200

    fixed example
    assumption that one could start multiple workers from a single ruby script is wrong -- should be abilities instead

--- examples/calculus_worker.rb
+++ examples/calculus_worker.rb
@@ -4,51 +4,42 @@ require '../lib/gearman'
 
 #Gearman::Util.debug = true
 
-# Additon worker
-add_worker = Gearman::Worker.new('localhost')
-add_worker.reconnect_sec = 2
-add_worker.add_ability('addition') do |data,job|
+worker = Gearman::Worker.new('localhost')
+worker.reconnect_sec = 2
+
+# Additon ability
+worker.add_ability('addition') do |data,job|
   values = Marshal.load(data)
   puts "[addition_worker] Calculating #{values.inspect}..."
-  sleep 5
+  # sleep 5
   values.first + values.last
 end
 
-# Subtraction worker
-sub_worker = Gearman::Worker.new('localhost')
-sub_worker.reconnect_sec = 2
-sub_worker.add_ability('subtraction') do |data,job|
+# Subtraction ability
+worker.add_ability('subtraction') do |data,job|
   values = Marshal.load(data)
   puts "[subtraction_worker] Calculating #{values.inspect}..."
-  sleep 5
+  # sleep 5
   values.first - values.last
 end
 
 # Multiplication worker
-mul_worker = Gearman::Worker.new('localhost')
-mul_worker.reconnect_sec = 2
-mul_worker.add_ability('multiplication') do |data,job|
+worker.add_ability('multiplication') do |data,job|
   values = Marshal.load(data)
   puts "[multiplication_worker] Calculating #{values.inspect}..."
-  sleep 5
+  # sleep 5
   values.first * values.last
 end
 
 # Division worker
-div_worker = Gearman::Worker.new('localhost')
-div_worker.reconnect_sec = 2
-div_worker.add_ability('division') do |data,job|
+worker.add_ability('division') do |data,job|
   values = Marshal.load(data)
   puts "[division_worker] Calculating #{data.inspect}..."
-  sleep 5
+  # sleep 5
   values.first / values.last
 end
 
 # Running the workers
-loop do 
-  puts '[worker] Starting workers...'
-  add_worker.work
-  sub_worker.work
-  mul_worker.work
-  div_worker.work
+loop do
+  worker.work
 end
