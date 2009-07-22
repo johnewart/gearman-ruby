4d6b75290cce2fda9ea2991ff316f141f2a94793
Kim Altintop <kim.altintop@xing.com>
2009-07-22 15:16:35 +0200

    comment on option_request("exceptions")

--- examples/client_exception.rb
+++ examples/client_exception.rb
@@ -5,7 +5,8 @@ Gearman::Util.debug = true
 servers = ['localhost:4730']
 
 client = Gearman::Client.new(servers)
-#try this out
+# make sure your gearmand supports propagation of worker exceptions!
+# (standard impl. doesn't -- use XING fork from http://github.com/xing/gearman-server)
 client.option_request("exceptions")
 
 taskset = Gearman::TaskSet.new(client)
