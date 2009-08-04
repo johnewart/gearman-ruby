f4bc5351415d9318dba66ad7825b34ad778c8d51
Kim Altintop <kim.altintop@xing.com>
2009-08-04 10:02:10 +0200

    put integer->symbol mappings for protocol in order of spec

--- lib/gearman/util.rb
+++ lib/gearman/util.rb
@@ -15,43 +15,42 @@ class Util
   # Map from Integer representations of commands used in the network
   # protocol to more-convenient symbols.
   COMMANDS = {
-    1  => :can_do,          # W->J: FUNC
-    23 => :can_do_timeout,  # W->J: FUNC[0]TIMEOUT
-    2  => :cant_do,         # W->J: FUNC
-    3  => :reset_abilities, # W->J: --
-    22 => :set_client_id,   # W->J: [RANDOM_STRING_NO_WHITESPACE]
-    4  => :pre_sleep,       # W->J: --
-
-    6  => :noop,            # J->W: --
-    7  => :submit_job,      # C->J: FUNC[0]UNIQ[0]ARGS
-    33 => :submit_job_low,     # C->J: FUNC[0]UNIQ[0]ARGS
-    34 => :submit_job_low_bg,  # C->J: FUNC[0]UNIQ[0]ARGS
-    21 => :submit_job_high,    # C->J: FUNC[0]UNIQ[0]ARGS
-    32 => :submit_job_high_bg, # C->J: FUNC[0]UNIQ[0]ARGS
-    18 => :submit_job_bg,      # C->J: FUNC[0]UNIQ[0]ARGS
-
-    8  => :job_created,     # J->C: HANDLE
-    9  => :grab_job,        # W->J: --
-    10 => :no_job,          # J->W: --
-    11 => :job_assign,      # J->W: HANDLE[0]FUNC[0]ARG
-
-    12 => :work_status,     # W->J/C: HANDLE[0]NUMERATOR[0]DENOMINATOR
-    13 => :work_complete,   # W->J/C: HANDLE[0]RES
-    14 => :work_fail,       # W->J/C: HANDLE
-    29 => :work_warning,    # W->J/C: HANDLE[0]MSG
-
-    25 => :work_exception,  # W->J: HANDLE[0]ARG
-    26 => :option_req,      # C->J: TEXT
-    27 => :option_res,      # J->C: TEXT
-
-    15 => :get_status,      # C->J: HANDLE
-    20 => :status_res,      # C->J: HANDLE[0]KNOWN[0]RUNNING[0]NUM[0]DENOM
-
-    25 => :work_exception,  # W->J/C: HANDLE[0]ARG
-    16 => :echo_req,        # ?->J: TEXT
-    17 => :echo_res,        # J->?: TEXT
-
-    19 => :error,           # J->?: ERRCODE[0]ERR_TEXT
+    1  => :can_do,               # W->J: FUNC
+    2  => :cant_do,              # W->J: FUNC
+    3  => :reset_abilities,      # W->J: --
+    4  => :pre_sleep,            # W->J: --
+    #5 =>  (unused),             # -      -
+    6  => :noop,                 # J->W: --
+    7  => :submit_job,           # C->J: FUNC[0]UNIQ[0]ARGS
+    8  => :job_created,          # J->C: HANDLE
+    9  => :grab_job,             # W->J: --
+    10 => :no_job,               # J->W: --
+    11 => :job_assign,           # J->W: HANDLE[0]FUNC[0]ARG
+    12 => :work_status,          # W->J/C: HANDLE[0]NUMERATOR[0]DENOMINATOR
+    13 => :work_complete,        # W->J/C: HANDLE[0]RES
+    14 => :work_fail,            # W->J/C: HANDLE
+    15 => :get_status,           # C->J: HANDLE
+    16 => :echo_req,             # ?->J: TEXT
+    17 => :echo_res,             # J->?: TEXT
+    18 => :submit_job_bg,        # C->J: FUNC[0]UNIQ[0]ARGS
+    19 => :error,                # J->?: ERRCODE[0]ERR_TEXT
+    20 => :status_res,           # C->J: HANDLE[0]KNOWN[0]RUNNING[0]NUM[0]DENOM
+    21 => :submit_job_high,      # C->J: FUNC[0]UNIQ[0]ARGS
+    22 => :set_client_id,        # W->J: [RANDOM_STRING_NO_WHITESPACE]
+    23 => :can_do_timeout,       # W->J: FUNC[0]TIMEOUT
+    24 => :all_yours,            # REQ    Worker
+    25 => :work_exception,       # W->J: HANDLE[0]ARG
+    26 => :option_req,           # C->J: TEXT
+    27 => :option_res,           # J->C: TEXT
+    28 => :work_data,            # REQ    Worker
+    29 => :work_warning,         # W->J/C: HANDLE[0]MSG
+    30 => :grab_job_uniq,        # REQ    Worker
+    31 => :job_assign_uniq,      # RES    Worker
+    32 => :submit_job_high_bg,   # C->J: FUNC[0]UNIQ[0]ARGS
+    33 => :submit_job_low,       # C->J: FUNC[0]UNIQ[0]ARGS
+    34 => :submit_job_low_bg,    # C->J: FUNC[0]UNIQ[0]ARGS
+    35 => :submit_job_sched,     # REQ    Client
+    36 => :submit_job_epoch      # REQ    Client
   }
 
   # Map e.g. 'can_do' => 1
