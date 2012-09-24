5b7f6f33bdaab624a24a49b2d586ff6e2bc59c9c
zhuangbiaowei <zhuangbiaowei@gmail.com>
2012-09-24 10:16:41 +0800

    Update examples/client.rb
    at line 11, if not '20' will get:
    
    TypeError: can't convert Fixnum into String
    from /usr/local/lib/ruby/gems/1.9.1/gems/gearman-ruby-3.0.4/lib/gearman/task.rb:179:in `+'

--- examples/client.rb
+++ examples/client.rb
@@ -8,7 +8,7 @@ servers = ['localhost:4730', 'localhost:4731']
 client = Gearman::Client.new(servers)
 taskset = Gearman::TaskSet.new(client)
 
-task = Gearman::Task.new('sleep', 20)
+task = Gearman::Task.new('sleep', '20')
 task.on_complete {|d| puts d }
 
 taskset.add_task(task)
